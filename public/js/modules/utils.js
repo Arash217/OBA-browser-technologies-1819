296e326b39103f0feafeb2a6e59257663d452bc6
Arash Paknezad <a.paknezad@hotmail.com>
2019-03-04 20:30:37 +0100

    formatting

--- public/js/modules/utils.js
+++ public/js/modules/utils.js
@@ -1,5 +1,4 @@
 export const xmlToJson = xml => {
-    // converting to DOM Tree
     const parser = new DOMParser();
     const srcDOM = parser.parseFromString(xml, "application/xml");
     return domToJson(srcDOM);
@@ -8,20 +7,15 @@ export const xmlToJson = xml => {
 const domToJson = srcDOM => {
     const children = [...srcDOM.children];
 
-    // base case for recursion.
     if (!children.length) {
         return srcDOM.innerHTML
     }
 
-    // initializing object to be returned.
     const jsonResult = {};
 
     for (const child of children) {
-
-        // checking is child has siblings of same name.
         const childIsArray = children.filter(eachChild => eachChild.nodeName === child.nodeName).length > 1;
 
-        // if child is array, save the values as array, else as strings.
         if (childIsArray) {
             if (jsonResult[child.nodeName] === undefined) {
                 jsonResult[child.nodeName] = [domToJson(child)];
