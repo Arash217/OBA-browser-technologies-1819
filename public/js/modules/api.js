588e11d5dede21a69ba221b0ee8a3b248b373456
Arash Paknezad <a.paknezad@hotmail.com>
2019-03-06 14:56:04 +0100

    added sound player

--- public/js/modules/api.js
+++ public/js/modules/api.js
@@ -1,8 +1,8 @@
 import * as utils from './utils.js';
 import config from './config.js';
 
-export const get = async () => {
-    const url = 'http://134.209.89.240:3000/singleSearch/singleSearch.xml?q=9789080940543&resultCount=1';
+export const getTracksByAlbumISBN = async ISBN => {
+    const url = `http://134.209.89.240:3000/singleSearch/singleSearch.xml?q=${ISBN}&resultCount=1`;
 
     const headers = new Headers({
         Authorization: `Basic ${btoa(config.username + ':' + config.password)}`
@@ -15,6 +15,32 @@ export const get = async () => {
     }
 
     const xml = await res.text();
+    const jsonData = utils.xmlToJson(xml);
 
-    return utils.xmlToJson(xml);
+    const {Album} = jsonData.Result.Popular.Albums;
+
+    return await getTracks(Album);
+};
+
+const getTracks = async album => {
+    const url = `http://localhost:3000/album/${album.AlbumTitle}`;
+
+    const res = await fetch(url);
+
+    if (!res.ok){
+        console.log(res);
+        throw Error('Something went wrong')
+    }
+
+    const data = await res.json();
+
+    const albumAndTracks = {
+        tracks: data.items,
+        album: album[0]
+    };
+
+    const { Cover } =  albumAndTracks.album;
+    albumAndTracks.album.Cover = Cover.replace('PICO', 'SUPERLARGE');
+
+    return albumAndTracks;
 };
--- public/js/modules/pages/details.js
+++ public/js/modules/pages/details.js
@@ -10,24 +10,60 @@ class Details extends DOM {
         this.contentId = '#details-page-content';
 
         DOM.registerPage(this.id);
+
+        this.eventListeners = [
+            element => {
+                const clickEventHandler = ({target}) => {
+                    if (target.classList.contains('track')) {
+                        const audio = target.querySelector('audio');
+
+                        if (audio.duration > 0 && !audio.paused) {
+                            audio.pause();
+                            audio.currentTime = 0;
+                        } else {
+                            audio.play();
+                        }
+                    }
+                };
+
+                element.addEventListener('click', clickEventHandler);
+            },
+        ]
     }
 
     template() {
-        return undefined;
+        return `
+            <span class="album-title">
+                {{ this.album.AlbumTitle }}
+            </span>
+            <div id="tracks">
+                {{#each this.tracks}}
+                    <div id="{{this.id}}" class="track">
+                         <audio class="audio" src="{{this.preview_url}}"></audio>
+                         <div class="album">
+                            <img class="album-image" src="{{../this.album.Cover}}" alt="{{../this.album.AlbumTitle}}">
+                            <span>{{this.name}}</span>
+                        </div>
+                    </div>
+                {{/each}}
+            </div>
+        `;
     }
 
     async shown(code) {
+        this.initEventListeners();
         try {
             this.display();
-            const res = await api.get();
 
-            console.log(res);
-            const { AlbumTitle } = res.Result.Popular.Albums.Album;
+            const res = await api.getTracksByAlbumISBN(code);
 
-            console.log(AlbumTitle);
-            alert(AlbumTitle)
+            this.data = {
+                albumAndTrack: res
+            };
+
+            this.renderContent(res);
         } catch (e) {
-            alert(e.message);
+            console.log(e.message);
         }
     }
 
