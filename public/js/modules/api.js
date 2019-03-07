a03654b1c72f19084ea1701bf285f442420ac193
Arash Paknezad <a.paknezad@hotmail.com>
2019-03-07 16:55:26 +0100

    get spotify data

--- public/js/modules/api.js
+++ public/js/modules/api.js
@@ -1,6 +1,8 @@
 import * as utils from './utils.js';
 import config from './config.js';
 
+let token = null;
+
 export const getTracksByAlbumISBN = async ISBN => {
     const url = `http://134.209.89.240:3000/singleSearch/singleSearch.xml?q=${ISBN}&resultCount=1`;
 
@@ -17,8 +19,6 @@ export const getTracksByAlbumISBN = async ISBN => {
     const xml = await res.text();
     const jsonData = utils.xmlToJson(xml);
 
-    console.log(jsonData);
-
     let data = jsonData.Result.Popular.Albums.Album;
 
     if (!data){
@@ -28,7 +28,30 @@ export const getTracksByAlbumISBN = async ISBN => {
     return await getTracks(data);
 };
 
+const getSpotifyToken = async () => {
+    const client_id = '71275f4b1eb54fdc969578e0af48fbe3';
+    const client_secret = '08645c05ab8b46f5af323c8f7bda2b98';
+
+    const formData = new URLSearchParams();
+    formData.append('grant_type', 'client_credentials');
+
+    const res = await fetch('http://127.0.0.1:3001/accounts/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+        },
+        body: formData.toString(),
+        json: true
+    });
+
+    const data = await res.json();
+    token = data.access_token;
+};
+
 const getTracks = async album => {
+
+    await getSpotifyToken();
 
     const albumTitle = album.AlbumTitle;
     let artist = '';
@@ -37,22 +60,51 @@ const getTracks = async album => {
         artist = album.Performers.Performer.PresentationName;
     }
 
-    const url = `http://134.209.89.240:3000/album/${encodeURIComponent(albumTitle)}/artist/${encodeURIComponent(artist)}`;
-    const res = await fetch(url);
-
-    if (!res.ok) {
-        throw Error('Something went wrong')
-    }
-
-    const data = await res.json();
+    const spotifyAlbum = await getSpotifyAlbum(albumTitle, artist);
+    console.log(spotifyAlbum);
+    const tracks = await getSpotifyTracks(spotifyAlbum.id);
 
     const albumAndTracks = {
-        tracks: data.items,
-        album: album
+        tracks: tracks.items,
+        album
     };
 
     const {Cover} = albumAndTracks.album;
     albumAndTracks.album.Cover = Cover.replace('PICO', 'SUPERLARGE');
 
     return albumAndTracks;
+};
+
+const getSpotifyAlbum = async (albumTitle, artist) => {
+    const res = await fetch(`http://127.0.0.1:3001/api/v1/search?q=${encodeURIComponent(albumTitle)}+artist:${encodeURIComponent(artist)}&type=album&limit=1`, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true
+    });
+
+    const data = await res.json();
+
+    let spotifyAlbum = data.albums.items[0];
+
+    console.log(data);
+
+    // if (data.albums.length > 0){
+    //     spotifyAlbum = data.albums.items[0];
+    // } else {
+    //     throw Error('Something went wrong');
+    // }
+
+    return spotifyAlbum;
+};
+
+const getSpotifyTracks = async albumId => {
+    const res = await fetch(`http://127.0.0.1:3001/api/v1/albums/${albumId}/tracks`, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true
+    });
+
+    return res.json();
 };
